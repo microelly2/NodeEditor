030dcaa6eee687cd922d504511a0c32f3defd9f5
microelly2 <thomas@freecadbuch.de>
2019-10-07 20:00:52 +0200

    utils.py

---
+++ nodeeditor/utils.py
@@ -0,0 +1,13 @@
+# common utilities for use in many files
+
+'''
+from nodeeditor.utils import *
+
+'''
+
+
+
+# reload for development
+import sys
+if sys.version_info[0] !=2:
+	from importlib import reload
